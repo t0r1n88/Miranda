f6134d0b5ee21f6aa02250cc5421cfef10374136
Oleg Budaev <t0r1n88@mail.ru>
2020-12-24 12:09:52 +0800

    Сделано

--- only_merge_sheet.py
+++ only_merge_sheet.py
@@ -1,52 +1,37 @@
-import xlwings as xw
 import os
-import time
-# Константы
-PATH_TO_FILES = 'data'
+import xlwings as xw
 
+path = 'data'
+files = []
+# Перебираем файлы в указанной директории
+for file in os.listdir(path):
+    if not file.startswith('~$') and file.endswith('.xlsx'):
+        files.append(file)
 
-def give_list_files(path, extension='xlsx'):
-    """
-    Функция для получения списка файлов с заданными расширениями и без временных файлов
-    :param path: Путь к файлам
-    :return: Список с файлами
-    """
-    # Создаем список где будут хранится найденые пути до файлов
-    files = []
-    # Перебираем файлы в указанной директории
-    for file in os.listdir(path):
-        if not file.startswith('~$') and file.endswith(extension):
-            files.append(file)
-    return files
-
-
+counter = 0
 path_master = 'master.xlsx'
+while counter < len(files):
+    master_workbook = xw.Book(path_master)
+    try:
+        for i in range(10):
+            name_sheet = files[counter][:-5]
+            if len(name_sheet) > 30:
+                name_sheet = name_sheet[:30]
 
+            append_workbook = xw.Book(f'data/{files[counter]}')
+            append_workbook.sheets(1).name = name_sheet
 
+            # Получаем лист из добавляемого файла
+            append_sheet = append_workbook.sheets(1)
+            #
+            append_sheet.api.Copy(After=master_workbook.sheets(1).api)
+            counter += 1
+    except IndexError:
+        # да я знаю что выглядит не очень
+        master_workbook.save()
+        master_workbook.app.quit()
+        break
 
-
-
-files = give_list_files(PATH_TO_FILES)
-# Открываем основную книгу
-master_workbook = xw.Book(path_master)
-for file in files:
-    name_sheet = file[:-5]
-    if len(name_sheet) > 30:
-        name_sheet = name_sheet[:30]
-
-    # открываем книгу которую будем добавлять
-    append_workbook = xw.Book(f'data/{file}')
-    append_workbook.sheets(1).name = name_sheet
-
-
-
-
-    # Получаем лист из добавляемого файла
-    append_sheet = append_workbook.sheets(1)
-    #
-    append_sheet.api.Copy(After=master_workbook.sheets(1).api)
-    time.sleep(15)
-    append_workbook.app.quit()
-master_workbook.save()
-master_workbook.app.quit()
+    master_workbook.save()
+    master_workbook.app.quit()
 
--- temp.py
+++ temp.py
@@ -1,28 +1,38 @@
-import xlwings as xw
 import os
-import openpyxl
+import xlwings as xw
 
+path = 'data'
+files = []
+# Перебираем файлы в указанной директории
+for file in os.listdir(path):
+    if not file.startswith('~$') and file.endswith('.xlsx'):
+        files.append(file)
 
+counter = 0
+path_master = 'master.xlsx'
+while counter < len(files):
+    master_workbook = xw.Book(path_master)
+    try:
+        for i in range(10):
+            name_sheet = files[counter][:-5]
+            if len(name_sheet) > 30:
+                name_sheet = name_sheet[:30]
 
+            append_workbook = xw.Book(f'data/{files[counter]}')
+            append_workbook.sheets(1).name = name_sheet
 
+            # Получаем лист из добавляемого файла
+            append_sheet = append_workbook.sheets(1)
+            #
+            append_sheet.api.Copy(After=master_workbook.sheets(1).api)
+            counter += 1
+            print(counter)
+    except IndexError:
+        # да я знаю что выглядит не очень
+        master_workbook.save()
+        master_workbook.app.quit()
+        break
 
+    master_workbook.save()
+    master_workbook.app.quit()
 
-
-path1 = '1.xlsx'
-path2 = 'master.xlsx'
-
-
-path3 = '3.xlsx'
-
-
-wb1 = xw.Book(path1)
-wb2 = xw.Book(path2)
-
-ws1 = wb1.sheets(1)
-ws1.api.Copy(Before=wb2.sheets(1).api)
-
-wb3 = xw.Book(path3)
-ws3 = wb3.sheets(1)
-ws3.api.Copy(Before=wb2.sheets(1).api)
-wb2.save()
-wb2.app.quit()
